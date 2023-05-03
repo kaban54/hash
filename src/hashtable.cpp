5bfcc68cff0094328e7c1f086cba4b9d2ea62b84
kaban54 <tikhonov.as@phystech.edu>
2023-05-03 21:43:34 +0300

    added insert

--- src/hashtable.cpp
+++ src/hashtable.cpp
@@ -4,11 +4,11 @@
 
 void HashTableCtor (HashTable *htable, size_t (*hash_func)(const char *elem), size_t size)
 {
-    assert (htable != nullptr);
+    assert (htable    != nullptr);
     assert (hash_func != nullptr);
 
+    htable -> hash_func = hash_func;
     htable -> size = size;
-    htable -> hash = hash_func;
     htable -> data = (HashTableElem **) calloc (size, sizeof (htable -> data[0]));
 
     assert (htable -> data != nullptr);
@@ -22,7 +22,7 @@ void HashTableDtor (HashTable *htable)
     free (htable -> data);
 
     htable -> size = 0;
-    htable -> hash = nullptr;
+    htable -> hash_func = nullptr;
 }
 
 void hashtable_list_dtor (HashTableElem *elem)
@@ -35,4 +35,35 @@ void hashtable_list_dtor (HashTableElem *elem)
 
         elem = next;
     }
+}
+
+void HashTableInsert (HashTable *htable, const char *value)
+{
+    assert (htable              != nullptr);
+    assert (htable -> hash_func != nullptr);
+    assert (htable -> data      != nullptr);
+    assert (value               != nullptr);
+
+    size_t hash = htable -> hash_func (value) % htable -> size;
+
+    htable -> data [hash] = hashtable_list_insert (htable -> data [hash], value);
+}
+
+HashTableElem *hashtable_list_insert (HashTableElem *elem, const char *value)
+{
+    if (hashtable_list_find (elem, value) != nullptr) elem;
+
+    HashTableElem *newelem = (HashTableElem *) calloc (1, sizeof (HashTableElem));
+    assert (newelem != nullptr);
+
+    newelem -> value = value;
+    newelem -> next = elem;
+
+    return newelem;
+}
+
+HashTableElem *hashtable_list_find (HashTableElem *elem, const char *value)
+{
+    while (elem && strcmp (elem -> value, value)) elem = elem -> next;
+    return elem;
 }
--- src/hashtable.h
+++ src/hashtable.h
@@ -4,20 +4,21 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 const size_t MAX_WORD_LEN = 64;
 
 
 struct HashTableElem
 {
-    char value [MAX_WORD_LEN];
+    const char *value;
     HashTableElem *next;
 };
 
 
 struct HashTable
 {
-    size_t (*hash)(const char *elem);
+    size_t (*hash_func)(const char *elem);
     HashTableElem **data;
     size_t size;
 };
@@ -28,5 +29,11 @@ void HashTableCtor (HashTable *htable, size_t (*hash_func)(const char *elem), si
 void HashTableDtor (HashTable *htable);
 
 void hashtable_list_dtor (HashTableElem *elem);
+
+void HashTableInsert (HashTable *htable, const char *value);
+
+HashTableElem *hashtable_list_insert (HashTableElem *elem, const char *value);
+
+HashTableElem *hashtable_list_find (HashTableElem *elem, const char *value);
 
 #endif
